48651e3675c00ca73fb898250ea52c9123830d1d
Szűcs Dóra <szucs.dora93@gmail.com>
2018-01-27 20:47:40 +0100

    getterek és setterek beállítása a todo és a user php fájlokban

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -15,6 +15,60 @@ class Todo
      * @ORM\Column(type="integer")
      */
     private $id;
-
-    // add your own fields
+     public function getId()
+     {
+       return $this->id;
+     }
+     public function setId($id)
+    {
+        $this->id = $id;
+    }
+     /**
+      * @ORM\Column(type="integer", length=11, name="user_id"))
+      */
+    private $userId;
+    public function getuserId()
+    {
+      return $this->user_id;
+    }
+    public function setuserName($user_id)
+    {
+        $this->userName = $user_name;
+    }
+    /**
+     * @ORM\Column(type="string", length=64, name="task_name")
+     */
+   private $taskName;
+   public function gettaskName()
+   {
+     return $this->task_name;
+   }
+   public function settaskName($task_name)
+    {
+        $this->taskName = $task_name;
+    }
+   /**
+    * @ORM\Column(type="dateTime", name="deadline")
+    */
+  private $deadline;
+  public function getdeadline()
+  {
+    return $this->deadline;
+  }
+  public function setdeadline($deadline)
+    {
+        $this->deadline = $deadline;
+    }
+   /**
+    * @ORM\Column(type="boolean", length=1, name="is_done")
+    */
+  private $isDone;
+  public function getisDone()
+  {
+    return $this->is_done;
+  }
+  public function setName($isDone)
+    {
+        $this->$isDone = $is_done;
+    }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+* @ORM\Table(name="users")
  */
 class User
 {
@@ -15,6 +16,65 @@ class User
      * @ORM\Column(type="integer")
      */
     private $id;
+    public function getId()
+    {
+        return $this->id;
+    }
 
-    // add your own fields
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+     /**
+      * @ORM\Column(type="string", length=128, name="full_name")
+      */
+     private $fullName;
+     public function getfullName()
+    {
+        return $this->full_name;
+    }
+
+    public function setfullName($full_name)
+    {
+        $this->fullName = $full_name;
+    }
+    /**
+    * @ORM\Column(type="string", length=32, name="user_name")
+    */
+     private $userName;
+     public function getuserName()
+    {
+        return $this->user_name;
+    }
+
+    public function setuserName($user_name)
+    {
+        $this->userName = $user_name;
+    }
+   /**
+    * @ORM\Column(type="string", length=128, name="e_mail_address")
+    */
+     private $eMailAddress;
+     public function geteMailAddress()
+    {
+        return $this->e_mail_address;
+    }
+
+    public function seteMailAddress($e_mail_address)
+    {
+        $this->$eMailAddress = $e_mail_address;
+    }
+   /**
+    * @ORM\Column(type="string", length=64, name="password")
+    */
+     private $password;
+     public function getpassword()
+    {
+        return $this->password;
+    }
+
+    public function setpassword($password)
+    {
+        $this->password = $password;
+    }
 }
